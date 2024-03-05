58be5605df42be1e3079c998b6793a7903932bc1
Nikita_IT_2004 <112660966+NikitaChemeris@users.noreply.github.com>
2024-03-05 23:00:24 +0100

    Added task of block 3

--- Magic methods. Double underscore (dunder) methods/02 Magic methods __add__, __mul__, __sub__ and __truediv__/task.py
+++ Magic methods. Double underscore (dunder) methods/02 Magic methods __add__, __mul__, __sub__ and __truediv__/task.py
@@ -1,13 +1,30 @@
 class Vektor:
-
+    
     def __init__(self, *args):
         for symvol in args:
             if not isinstance(symvol, int):
                 args.remove(symvol)
         
         self.values = sorted(args)
-
+    
     def __str__(self):
         if len(self.values) > 0:
-            return self.values
-        
+            return f"Vektor{tuple(self.values)}"
+        else:
+            return "Empty Vektor"
+    
+    def __add__(self, other):
+        if isinstance(other, Vektor):
+            if len(self.values) == len(other.values):
+                self.new_values = []
+                for i in range(len(self.values)):
+                    self.new_values.append(self.values[i] + other.values[i])
+                return f"Vektor{tuple(self.new_values)}"
+            else:
+                return "Multiplication of vectors of different lengths is not available"
+        if isinstance(other, int):
+            for i in range(len(self.values)):
+                self.values[i] += other
+            return f"Vektor{tuple(self.values)}"
+        else:
+            return "Vector cannot be added to <value>"
