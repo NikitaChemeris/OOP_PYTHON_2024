dcb602a7ee8b41237d6e8265c53801c765d10e1c
Nikita_IT_2004 <112660966+NikitaChemeris@users.noreply.github.com>
2024-03-07 08:20:28 +0100

    Added task.py

---
+++ Magic methods. Double underscore (dunder) methods/03 Special methods of comparing objects of classes/task.py
@@ -0,0 +1,44 @@
+class ChessPlayer:
+
+    def __init__(self, name, surname, rating):
+        self.name = name
+        self.surname = surname
+        self.rating = rating
+
+    def __eq__(self, other):
+        if isinstance(other, int):
+            return self.rating == other
+        elif isinstance(other, ChessPlayer):
+            return self.rating == other.rating
+        else:
+            return "Unable to perform a comparison"
+        
+    def __gt__(self, other):
+        if isinstance(other, int):
+            return self.rating > other
+        elif isinstance(other, ChessPlayer):
+            return self.rating > other.rating
+        else:
+            return "Unable to perform a comparison"
+        
+    def __lt__(self, other):
+        if isinstance(other, int):
+            return self.rating < other
+        elif isinstance(other, ChessPlayer):
+            return self.rating < other.rating
+        else:
+            return "Unable to perform a comparison"
+    
+
+"""
+Example 
+
+magnus = ChessPlayer ('Carlsen', 'Magnus', 2847)
+ian = ChessPlayer ('Ian', 'Nepomniachtchi', 2789)
+print(magnus == 4000)
+print(ian == 2789)
+print(magnus == ian)
+print(magnus > ian)
+print(magnus < ian)
+print (magnus < [1, 2])
+"""
